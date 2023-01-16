0812297705347ab1b253d56af14791cdf8bfac2e
jacklindev <jackliuprodev@gmail.com>
2023-01-16 16:04:06 -0500

    minor style change

--- src/App.js
+++ src/App.js
@@ -44,6 +44,9 @@ const App = () => {
   return (
     <div className="flex justify-center items-center w-full h-screen">
       <div className="flex flex-col items-start gap-4 w-96 rounded border-1 border-solid border-gray-300 p-8 shadow">
+        <h3 className="w-full font-bold text-center text-2xl">
+          Generate a Roman Numeral
+        </h3>
         <div className="flex gap-2 items-center w-full">
           <label htmlFor="number">Number:</label>
           <input
@@ -56,11 +59,18 @@ const App = () => {
           />
         </div>
         <div className="flex gap-2 items-center">
-          <label htmlFor="number">Roman Numeral:</label>
-          <input type="text" readOnly value={romanNumerals} />
+          <label htmlFor="number" className="whitespace-nowrap">
+            Roman Numeral:
+          </label>
+          <input
+            type="text"
+            readOnly
+            value={romanNumerals}
+            className="py-2 px-2 bg-transparent rounded-md text-2xl text-green-900 focus:outline-none"
+          />
         </div>
         <button
-          className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"
+          className="w-full bg-blue-500 hover:bg-blue-700 text-white mt-4 py-2 px-4 rounded"
           onClick={onGenerate}
         >
           Generate
